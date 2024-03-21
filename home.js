6a75a8d3ff7aaedd7103b10eb90b00eff0b30c84
partage2connaissance <partage2connaissance@gmail.com>
2024-03-21 15:08:09 +0100

    a

--- home.js
+++ home.js
@@ -11,6 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
         updateHoveredItem();
     });
 
+    // Charger la base de données JSON
+    fetch('bdd.json')
+        .then(response => response.json())
+        .then(data => {
+            // Parcourir les éléments de la base de données et créer les éléments HTML
+            const list = document.querySelector('.spotify-playlists .list');
+            data.forEach(item => {
+                const newItem = document.createElement('div');
+                newItem.classList.add('item');
+                newItem.innerHTML = `
+                    <a href="#">
+                        <h4>${item.title}</h4>
+                    </a>
+                `;
+                list.appendChild(newItem);
+            });
+
+            // Mettre à jour les éléments survolés après le chargement de la base de données
+            updateHoveredItem();
+        })
+        .catch(error => console.error('Erreur lors du chargement de la base de données:', error));
+
     function updateHoveredItem() {
         itemList.forEach(item => {
             const rect = item.getBoundingClientRect();
